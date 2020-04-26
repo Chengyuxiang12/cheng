af8cb2abd98e587ae40d140513f980af60f0ccd2
Chengyuxiang12 <1561933707@qq.com>
2020-04-26 11:56:26 +0800

    DEMO

--- maopao.py
+++ maopao.py
@@ -1,1 +1,7 @@
-print("cheng")
+list1 = [32, 3, 45, 2, 57, 13]
+for i in range(len(list1)-1):
+    for j in range(len(list1)-i-1):
+        if list1[j] > list1[j+1]:
+            list1[j], list1[j+1] = list1[j+1], list1[j]
+
+print(list1)
